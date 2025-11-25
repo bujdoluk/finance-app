96e5629ed97274b0a3fae8fa562d2c0f55d01d78
Lukáš Bujdoš <lukas.bujdos@gmail.com>
2025-11-25 17:14:36 +0100

    wip: fix

--- backend/src/__tests__/example.spec.ts
+++ backend/src/__tests__/example.spec.ts
@@ -3,7 +3,7 @@ import { describe, expect, it } from "vitest";
 import { sum } from "../utils/example";
 
 describe("sum function", () => {
-  it("should add two positive numbers correctly.", () => {
+  it("should add two positive numbers correctly", () => {
     expect(sum(1, 2)).toBe(3);
   });
 });
