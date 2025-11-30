54b8d892adb27a8cad6f6e9607fd5ff00fae6002
Lukáš Bujdoš <lukas.bujdos@gmail.com>
2025-11-30 20:24:17 +0100

    feat: added swagger docs for get all users endpoint

--- backend/index.ts
+++ backend/index.ts
@@ -3,12 +3,35 @@ import cors from 'cors';
 import morgan from 'morgan';
 import os from "os";
 import router from "./src/routes/routes";
+import swaggerUI from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
 
 const app = express();
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+
 app.use("/v1", router);
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Finance API",
+      version: "1.0.0",
+      description: "A simple personal finance tracking API"
+    },
+    servers: [
+      {
+        url: "http://localhost:3001"
+      }
+    ]
+  },
+  apis: ["./src/user/user-controller.ts"]
+}
+
+const specs = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 const port = Number(process.env.APP_PORT ?? process.env.PORT ?? "9001");
 
