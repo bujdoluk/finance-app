e36e3a0477eb1decd6a2cced7e9d8861ef34c29c
Lukáš Bujdoš <lukas.bujdos@gmail.com>
2025-12-02 19:25:22 +0100

    fix: fixed tsconfig absolute path

--- backend/src/eslint.config.js
+++ backend/src/eslint.config.js
@@ -2,28 +2,34 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import perfectionist from "eslint-plugin-perfectionist";
 import vitest from "@vitest/eslint-plugin";
+import { fileURLToPath } from "url";
+
+const tsconfigRootDir = fileURLToPath(new URL("../", import.meta.url));
 
 export default tseslint.config(
   {
     ignores: ["**/*.js"],
   },
+
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
   tseslint.configs.stylisticTypeChecked,
+
   {
     languageOptions: {
       parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+        project: "./tsconfig.json",
+        tsconfigRootDir,
+        sourceType: "module",
       },
     },
   },
+
   perfectionist.configs["recommended-natural"],
+
   {
     files: ["**/*.test.ts", "**/*.spec.ts"],
-    plugins: {
-      vitest,
-    },
+    plugins: { vitest },
     rules: {
       ...vitest.configs.recommended.rules,
       "@typescript-eslint/unbound-method": "off",
